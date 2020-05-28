a407b465b25ccc9bd0f37679efa5ed28abab18c6
Mortimer Snerd <Pat.Kelley@gmail.com>
2020-05-28 19:34:29 -0400

    Change handle hierarchy to not use inheritance.
    Having the more specific handles inherit from ManagedHandle caused
    a problem with arrays of handles.  Inheritence changes the memory
    layout, adding a member, so a seq[Instance] has a different layout
    than a seq of the raw OSPObject handle.  It's common to pass arrays of
    handles in to OSPRay, so the scoped way with ManagedHandle can not be
    passed as shared data.
    
    Trying to work around this by storing an array of raw OSPObject handles
    instead works, but opens you up to having to manually call retain and
    release for the handles again, negating the point of having
    ManagedHandle.
    
    Changed the implementation so instead of inheriting, the more specific
    objects include a ManagedHandle member. With a little template magic
    to paper over the structure differences, and add a converter that can
    decay a more specific instance to a ManagedHandle as a replacement for
    inheritance, it works well.

--- src/ospray.nim
+++ src/ospray.nim
@@ -6,33 +6,44 @@ import
 export ospray_raw
 
 type
-  ManagedHandle* = object of RootObj
+  ManagedHandle* = object
     ## Handle for an OSPRay object that will ospRelease()
     ## the underlying handle when it goes out of scope. 
     ## Also includes `set*()` functions for setting parameters
     ## on the handles.
     hndl: OSPObject
 
-  # 1 level hierarchy of handles - everything can cast to a ManagedHandle, but
-  # are otherwise not interchangable.
-  Camera* = object of ManagedHandle
-  Geometry* = object of ManagedHandle
-  Data* = object of ManagedHandle
-  Material* = object of ManagedHandle
-  GeometricModel* = object of ManagedHandle
-  Group* = object of ManagedHandle
-  Instance* = object of ManagedHandle
-  World* = object of ManagedHandle
-  Light* = object of ManagedHandle
-  Renderer* = object of ManagedHandle
-  FrameBuffer* = object of ManagedHandle
+  # 1 level hierarchy of handles - we don't use inheritance 
+  # because that would change the memory layout in a way that
+  # would make it so you could not pass a seq[ManagedObject]
+  # as shared data to OSPRay.  Instead, everything is a separate
+  # but otherwise identical object, with converters to allow
+  # these more specific types to decay back to their "super" type.
+  Camera* = object
+    inner: ManagedHandle
+  Geometry* = object
+    inner: ManagedHandle
+  Data* = object
+    inner: ManagedHandle
+  Material* = object
+    inner: ManagedHandle
+  GeometricModel* = object
+    inner: ManagedHandle
+  Group* = object
+    inner: ManagedHandle
+  Instance* = object
+    inner: ManagedHandle
+  World* = object
+    inner: ManagedHandle
+  Light* = object
+    inner: ManagedHandle
+  Renderer* = object
+    inner: ManagedHandle
+  FrameBuffer* = object
+    inner: ManagedHandle
 
-  #TODO one bad part of this hierarchy is that if you create
-  # a seq of Instances, you can't pass this as a shared array
-  # of instances to OSPRay (since there's extra data in the 
-  # Instance, thanks to inheritance).  Can work around
-  # this with a seq[OSPObject], but that is ugly.  May need to rethink
-  # the hierarchy.
+  #TODO outside of ManagedHandle, there should be a separate
+  # impl for OSPDevice.
 
   Library* = object
     ## Object that scopes the initialization and 
@@ -43,35 +54,43 @@ type
     ## that still call into the library.
     initialized: bool
 
-converter toOSPObject*(mh: ManagedHandle) : OSPObject = mh.hndl
-
 template declareTypeBoundFunctions(T: typedesc) : untyped = 
   ## Generates some type bound functions.  Noisy, but necessary since
   ## we made the handles a shallow type hierarchy.
+  template handle*(h: T) : OSPObject = 
+    when T is ManagedHandle:
+      h.hndl
+    else:
+      h.inner.hndl
+
   proc `$`*(h: T) : string = 
-    "[" & T.name & " " & repr(cast[pointer](h.hndl)) & "]"
-    
-  proc `=destroy`*(mh: var T) = 
-    if mh.hndl != nil:
-      when defined(loudDestroy):
-        echo "ospRelease " & T.name & " " & repr(cast[pointer](mh.hndl))
-      ospRelease(mh.hndl)
-      mh.hndl = nil
+    "[" & T.name & " " & repr(cast[pointer](h.handle)) & "]"
 
-  proc `=`*(a: var T; b: T) = 
-    if a.hndl != b.hndl:
-      `=destroy`(a)
-      a.hndl = b.hndl
-      ospRetain(a.hndl)
-      when defined(loudDestroy):
-        echo "retaining " & T.name & " " &  cast[pointer](a.hndl).repr
+  when T is ManagedHandle:
+    proc `=destroy`*(mh: var T) =
+      if mh.hndl != nil:
+        when defined(loudDestroy):
+          echo "ospRelease " & T.name & " " & repr(cast[pointer](mh.hndl))
+        ospRelease(mh.hndl)
+        mh.hndl = nil
 
-  proc `=sink`*(a: var T; b: T) = 
-    if a.hndl != b.hndl:
+    proc `=`*(a: var T; b: T) =
+      if a.hndl != b.hndl:
+        `=destroy`(a)
+        a.hndl = b.hndl
+        ospRetain(a.hndl)
+        when defined(loudDestroy):
+          echo "retaining " & T.name & " " &  cast[pointer](a.hndl).repr
+
+    proc `=sink`*(a: var T; b: T) =
       `=destroy`(a)
       a.hndl = b.hndl
       when defined(loudDestroy):
         echo "sinking " & T.name & " " &  cast[pointer](a.hndl).repr
+  else:
+    converter decayToHandle*(h: T) : ManagedHandle = h.inner
+
+    
 
 declareTypeBoundFunctions(ManagedHandle)
 declareTypeBoundFunctions(Camera)
@@ -101,7 +120,11 @@ template mkh(tname: typedesc; hnd0: untyped) : untyped =
   let newh = hnd0
   when defined(loudDestroy):
     echo "making " & tname.name & " " & cast[pointer](newh).repr
-  tname(hndl: newh)
+  ospRetain(hnd0)
+  when tname is ManagedHandle:
+    tname(hndl: newh)
+  else:
+    tname(inner: ManagedHandle(hndl: newh))
 
 proc retain*(h: ManagedHandle) = 
   ## If the handle is valid, bumps it's reference count up so it 
@@ -134,13 +157,13 @@ proc setObjectAsData*(h: ManagedHandle; name: string; mdl: GeometricModel) =
   ## Reminder - ospSetObjectAsData is a helper function for assigning a single
   ## item to an array, without having to create the array.  This is not the same
   ## as the other set* calls.
-  ospSetObjectAsData(h.hndl, name, OSPDataType.OSP_GEOMETRIC_MODEL, mdl.hndl)
+  ospSetObjectAsData(h.hndl, name, OSPDataType.OSP_GEOMETRIC_MODEL, mdl.handle)
 
 proc setObjectAsData*(h: ManagedHandle; name: string; inst: Instance) = 
-  ospSetObjectAsData(h.hndl, name, OSPDataType.OSP_INSTANCE, inst.hndl)
+  ospSetObjectAsData(h.hndl, name, OSPDataType.OSP_INSTANCE, inst.handle)
 
 proc setObjectAsData*(h: ManagedHandle; name: string; inst: Light) = 
-  ospSetObjectAsData(h.hndl, name, OSPDataType.OSP_LIGHT, inst.hndl)
+  ospSetObjectAsData(h.hndl, name, OSPDataType.OSP_LIGHT, inst.handle)
 
 proc init*(args: openarray[string]): (OSPError, seq[string], Library) = 
   ## Initializes the library.  On success, returns 
@@ -184,13 +207,13 @@ proc newMaterial*(rendererType: string; materialType: cstring): Material =
   mkh(Material, ospNewMaterial(rendererType, materialType))
 
 proc newGeometricModel*(a1: Geometry): GeometricModel = 
-  mkh(GeometricModel, ospNewGeometricModel(cast[OSPGeometry](a1.hndl)))
+  mkh(GeometricModel, ospNewGeometricModel(cast[OSPGeometry](a1.handle)))
 
 proc newGroup*(): Group = 
   mkh(Group, ospNewGroup())
 
 proc newInstance*(a1: Group): Instance = 
-  mkh(Instance, ospNewInstance(cast[OSPGroup](a1.hndl)))
+  mkh(Instance, ospNewInstance(cast[OSPGroup](a1.handle)))
 
 proc newWorld*() : World = 
   mkh(World, ospNewWorld())
@@ -199,7 +222,7 @@ proc newLight*(kind: string) : Light =
   mkh(Light, ospNewLight(kind))
 
 proc getBounds*(obj: World|Instance|Group) : OSPBounds = 
-  ospGetBounds(obj.hndl)
+  ospGetBounds(obj.handle)
 
 proc newRenderer*(kind: string) : Renderer = 
   mkh(Renderer, ospNewRenderer(kind))
@@ -210,19 +233,19 @@ proc newFrameBuffer*(size_x: cint; size_y: cint; format: OSPFrameBufferFormat;
                                       frameBufferChannels))
 
 proc resetAccumulation*(a1: FrameBuffer) = 
-  ospResetAccumulation(cast[OSPFrameBuffer](a1.hndl))
+  ospResetAccumulation(cast[OSPFrameBuffer](a1.handle))
 
 proc renderFrameBlocking*(a1: FrameBuffer; a2: Renderer; a3: Camera;
                           a4: World): cfloat = 
-  ospRenderFrameBlocking(cast[OSPFrameBuffer](a1.hndl), 
-                         cast[OSPRenderer](a2.hndl),
-                         cast[OSPCamera](a3.hndl),
-                         cast[OSPWorld](a4.hndl))
+  ospRenderFrameBlocking(cast[OSPFrameBuffer](a1.handle), 
+                         cast[OSPRenderer](a2.handle),
+                         cast[OSPCamera](a3.handle),
+                         cast[OSPWorld](a4.handle))
 
 proc mapFrameBuffer*[T](fb: FrameBuffer; channel: OSPFrameBufferChannel): ptr UncheckedArray[T] = 
-  cast[ptr UncheckedArray[T]](ospMapFrameBuffer(cast[OSPFrameBuffer](fb.hndl), 
+  cast[ptr UncheckedArray[T]](ospMapFrameBuffer(cast[OSPFrameBuffer](fb.handle), 
                                                 channel))
 
 
 proc unmapFrameBuffer*[T](mapped: ptr UncheckedArray[T]; fb: FrameBuffer) = 
-  ospUnmapFrameBuffer(mapped, cast[OSPFrameBuffer](fb.hndl))
+  ospUnmapFrameBuffer(mapped, cast[OSPFrameBuffer](fb.handle))
